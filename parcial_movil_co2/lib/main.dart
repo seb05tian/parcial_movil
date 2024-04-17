9afa73e14d4422b753fcccc2c537ff271be9f7ad
Hector Ruiz <ruizcadenahector@gmail.com>
2024-04-17 18:50:02 -0500

    guardar

--- parcial_movil_co2/lib/main.dart
+++ parcial_movil_co2/lib/main.dart
@@ -14,11 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  first_screen(),
+      home: first_screen(),
     );
   }
 }
@@ -37,25 +36,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-     
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
